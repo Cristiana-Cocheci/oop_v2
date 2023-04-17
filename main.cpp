ee0a47e467653266036dce9ef5302ca2c0d97297
Cristiana-Cocheci <cristiana.cocheci@gmail.com>
2023-04-17 10:48:51 +0300

    tot nu merge rlutil

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ public:
     }
     void move()
     {
-        if(rand()%10==1)
+        if(rand()%10==1) //10% sanse sa intre o masina pe drum
         {
             cars.push_front(true);
         }
@@ -30,12 +30,18 @@ public:
         cars.pop_back();
     }
     bool trackPosition(int pos){return cars[pos];}
+
+    //operator <<
+    friend std::ostream& operator<<(std::ostream& out, const Lane& l){
+        out<<"Afisare banda"<<"\n";
+        return out;
+    }
 };
 class Player{
 private:
     int x,y, noLanes, mapWidth;
 public:
-    Player(int width, int height){x=width/2;y=0, noLanes=height, mapWidth=width;}
+    Player(int width=20, int height=10){x=width/2;y=0, noLanes=height, mapWidth=width;}
     Player(const Player& other): x(other.x),y(other.y){};
     Player& operator=(const Player& other)
     {
@@ -43,6 +49,7 @@ public:
         y=other.y;
         return *this;
     }
+    ~Player(){}
     int getX() const{return x;}
     int getY() const{return y;}
     void MoveLeft(){if(x>0)x--;}
@@ -51,6 +58,12 @@ public:
     void MoveDown(){if(y>0)y--;}
     void reset(){y=0;x=mapWidth/2;}
 
+    //operator <<
+    friend std::ostream& operator<<(std::ostream& out, const Player& p){
+        out<<"Coordonatle x,y: "<<p.x<<" "<<p.y<<"\n";
+        return out;
+    }
+
 };
 class Game{
 private:
@@ -58,8 +71,8 @@ private:
     int noLanes;
     int mapWidth;
     int score;
-    Player *player;
-    std::vector <Lane*> map;
+    Player player;
+    std::vector <Lane> map;
 public:
     Game(int w=20, int h=10, int score_=0)
     {
@@ -67,11 +80,18 @@ public:
         noLanes=h;
         mapWidth=w;
         quit=false;
-        for(int i=0;i<noLanes;i++) {
-            map.push_back(new Lane(mapWidth));
-        }
-        player= new Player(mapWidth, noLanes);
+        //map=std::vector<Lane>(h);
 
+        for(int i=0;i<noLanes;i++) {
+            map.push_back(Lane(mapWidth));
+        }
+        player= Player(mapWidth, noLanes);
+
+    }
+    //operator <<
+    friend std::ostream& operator<<(std::ostream& out, const Game& g){
+        out<<"Date de baza joc: numar benzi-"<<g.noLanes<<", latimea drumului-"<<g.mapWidth<<", scorul-"<<g.score<<"\n";
+        return out;
     }
     void draw()
     {
@@ -84,9 +104,9 @@ public:
                 if(i==0 && (j==0 || j==mapWidth-1)){std::cout<<"-";}
                 if(i==0 && (j==1 || j==mapWidth-2)){std::cout<<"S";}
                 if(i==noLanes-1 && (j==0 || j==mapWidth-1)){std::cout<<"F";}
-                if(map[i]->trackPosition(j) && i>0 && i<noLanes-1)
+                if(map[i].trackPosition(j) && i>0 && i<noLanes-1)
                 { std::cout << "c "; }
-                else if(player->getX()==j && player->getY()==i)
+                else if(player.getX()==j && player.getY()==i)
                 { std::cout <<"P "; }
                 else
                 { std::cout<<" "; }
@@ -100,10 +120,10 @@ public:
         if(kbhit())
         {
             char current = getch();
-            if(current=='a'){player->MoveLeft();}
-            if(current=='w'){player->MoveUp();}
-            if(current=='s'){player->MoveDown();}
-            if(current=='d'){player->MoveRight();}
+            if(current=='a'){player.MoveLeft();}
+            if(current=='w'){player.MoveUp();}
+            if(current=='s'){player.MoveDown();}
+            if(current=='d'){player.MoveRight();}
             if(current=='q'){quit=true;}
         }
     }
@@ -112,19 +132,19 @@ public:
         {
             if(rand()%10==1)
             {
-                map[i]->move();
+                map[i].move();
             }
-            if(map[i]->trackPosition(player->getX()) && player->getY()==i) {
+            if(map[i].trackPosition(player.getX()) && player.getY()==i) {
                 quit = true;
                 std::cout << "YOU LOSE ;-(\n";
             }
         }
-        if(player->getY()==noLanes-1)
+        if(player.getY()==noLanes-1)
         {
             /*quit=true;
             std::cout<< "YOU WIN!!!!\n";*/
             score++;
-            player->reset();
+            player.reset();
         }
     }
     void run()
@@ -142,7 +162,5 @@ int main() {
     srand(time(NULL));
     Game joc(30,5);
     joc.run();
-    std::string aux;
-    std::cin>>aux;
     return 0;
 }
