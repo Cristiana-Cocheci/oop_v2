f164c2b33d85fb05eacf9a5332355a471dcd0ad4
Cristiana-Cocheci <cristiana.cocheci@gmail.com>
2023-04-24 09:59:19 +0300

    tot nu merge rlutil

--- main.cpp
+++ main.cpp
@@ -46,16 +46,16 @@ private:
 public:
     Player(int width=20, int height=10){x=width/2;y=0, noLanes=height, mapWidth=width;}
     Player(const Player& other): x(other.x),y(other.y), noLanes(other.noLanes), mapWidth(other.mapWidth){ std::cout<<"cc\n";};
-    Player& operator=(const Player& other)
-    {
-        std::cout<<"op=\n";
-        x=other.x;
-        y=other.y;
-        noLanes=other.noLanes;
-        mapWidth=other.mapWidth;
+    Player& operator=(const Player& other) {
+        std::cout << "op=\n";
+        x = other.x;
+        y = other.y;
+        noLanes = other.noLanes;
+        mapWidth = other.mapWidth;
         return *this;
     }
     ~Player(){std::cout<<"destr\n";}
+
     int getX() const{return x;}
     int getY() const{return y;}
     void MoveLeft(){if(x>0)x--;}
